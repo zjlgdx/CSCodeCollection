e5ebdd6d23ca0cb1e18dc5cfa1634f10502816b5
Yvan Yang <yvan.yang@perficientgdc.com.cn>
2012-05-02 17:00:48 +0800

    Add Cache Demo in ASP.NET

---
+++ CacheDemo/CacheDemo/CacheDemoPage.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Caching;
+
+namespace CacheDemo
+{
+    public partial class CacheDemoPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string result = string.Empty;
+
+            if (!IsPostBack)
+            {
+                result = DateTime.Now.ToString();
+                Cache.Insert("Trigger", DateTime.Now, null,
+    DateTime.Now.AddSeconds(60), Cache.NoSlidingExpiration);
+                CacheDependency cd = new CacheDependency(null,
+                new string[] { "Trigger" });
+                Cache.Insert("ActiveBugs", result, cd, Cache.NoAbsoluteExpiration, TimeSpan.FromSeconds(15));
+            }
+            
+            CachedItem.Text = Cache["ActiveBugs"].ToString();
+        }
+    }
+}
