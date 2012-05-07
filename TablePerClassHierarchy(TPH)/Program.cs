f6445fa27099ddea6e9480d2aeb18eefcc5f2f2d
Yvan <dlkts@126.com>
2012-05-07 21:08:12 +0800

    Add TablePerClassHierarchy Project

---
+++ TablePerClassHierarchy(TPH)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new TablePerHierarchy.VehicleDBEntities();
+            var q = (from c in db.Vehicles.OfType<TablePerHierarchy.Car>()
+                             select c);
+
+            var r = q.ToList();
+        }
+    }
+}
