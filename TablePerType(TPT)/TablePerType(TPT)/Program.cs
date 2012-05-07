7388997759b62106ca582affa6ba3b4ada8d872c
Yvan <dlkts@126.com>
2012-05-07 22:17:04 +0800

    add TablePerType TPT Project

---
+++ TablePerType(TPT)/TablePerType(TPT)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TablePerType_TPT_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new TablePerType.VehicleDBEntities();
+            var query = (from c in db.Vehicles.OfType<TablePerType.Car>()
+                             select c).ToList(); 
+        }
+    }
+}
