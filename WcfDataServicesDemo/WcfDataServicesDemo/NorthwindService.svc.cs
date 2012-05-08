4f5956ad051e599c75eee36b546223a2ef48bdcc
Yvan Yang <yvan.yang@perficientgdc.com.cn>
2012-05-08 16:52:20 +0800

    Add Wcf dataServices project

---
+++ WcfDataServicesDemo/WcfDataServicesDemo/NorthwindService.svc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace WcfDataServicesDemo
+{
+    public class NorthwindService : DataService<NorthwindEntities>
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+            // Examples:
+            // config.SetEntitySetAccessRule("MyEntityset", EntitySetRights.AllRead);
+            // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+            config.SetEntitySetAccessRule("*", EntitySetRights.All);
+            config.SetServiceOperationAccessRule("*",
+               ServiceOperationRights.All); 
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+        }
+
+        [WebGet]
+        public IQueryable<CustOrderHist_Result> CustOrderHist(string customerID)
+        {
+            using (NorthwindEntities db = new NorthwindEntities())
+            {
+                return db.CustOrderHist(customerID).ToList().AsQueryable();
+            }
+        }
+    }
+}
