6c6c8f63ab988d13c0f0e7afc560f5d868892c99
Yvan Yang <yvan.yang@perficientgdc.com.cn>
2012-05-09 10:46:34 +0800

    add SymmetricCryptography project

---
+++ SymmetricCryptography/SymmetricCryptography/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SymmetricCryptography
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SymmetricCryptographyUtil encryptUtil = new SymmetricCryptographyUtil();
+            var encryptResult = encryptUtil.SymmetricEncryptionResult();
+           var decryResult = encryptUtil.symmetricDecryption();
+        }
+    }
+}
